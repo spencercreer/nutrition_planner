2b0f121f4f2e3e5eff6234be0a16046c305b7d8c
spencercreer <spencercreer@gmail.com>
2021-02-19 07:06:41 -0700

    added required fields to form

--- nutrient_calculator.js
+++ nutrient_calculator.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    $('#calculateBtn').on('click', function(){
+    $('#calculateBtn').on('click', function(event){
+        event.preventDefault();
         let gender = 'male';
         let weight = $('#weight').val();
         let height = $('#height').val();
@@ -10,6 +11,8 @@ $(document).ready(function(){
         let goalBF = goalWeight * goalBFP/100;
         let bmr;
         let tdee;
+        let calDeficit;
+        let targetCal
         $('#goal-bf').attr('placeholder', goalBF);
         $('#goal-lbm').attr('placeholder', goalWeight- goalBF);
         // BMR = 370 + 21.6 x LBM(kg)
@@ -21,7 +24,11 @@ $(document).ready(function(){
             bmr = 10 * 0.45359 * weight + 6.25 * height * 2.54 - 5 * age - 161;         
         }
         tdee = 1.5 * bmr;
+        calDeficit = 20;
+        targetCal = tdee * (1 - calDeficit/100);
         $('#bmr').attr('placeholder', Math.round(bmr));
         $('#tdee').attr('placeholder', Math.round(tdee));
+        $('#cal-deficit').attr('placeholder', calDeficit);
+        $('#target-cal').attr('placeholder', Math.round(targetCal));
     });
 })
