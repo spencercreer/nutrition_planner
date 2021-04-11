364584a135176f173f983d8a4793b6281aff9087
spencercreer <spencercreer@gmail.com>
2021-04-10 21:32:01 -0600

    set up server, added mySQL database, included passport

---
+++ schema.sql
@@ -0,0 +1,5 @@
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '955503Sc!';
+flush privileges;
+DROP DATABASE IF EXISTS passport_demo;
+CREATE DATABASE passport_demo;
+USE passport_demo;
